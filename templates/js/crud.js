f890f42d3faaaaf00d90b690e8fad96448a15d8f
Maykon Granemann <maykondgranemann@olist.com>
2020-08-06 13:44:19 -0300

    Ajax Json

--- templates/js/crud.js
+++ templates/js/crud.js
@@ -3,29 +3,32 @@ function read_api(id){
 
 }
 function create_api(data){
-    console.log('asd')
     $('.msg.success').html('<p>Dado salvo com sucesso!</p>');
 }
 function update_api(data){
-
 }
 function delete_api(id){
-
+    $('.msg.success').html('<p>Dado deletado com sucesso!</p>');
 }
 //------------------ end crud api
 
 function loadResource(url, selector){
-    $(selector).load(url);
+    $(selector).load(url).promise().done(()=>{
+        index = url.indexOf('?')
+        if(index!=-1){
+            id = url.substring(index+4);
+            findById(id);
+        }
+    });
 }
 function btnClick(event){
     event.preventDefault();
     url = $(event.target).attr('href');
-    resource = '#content';
+    resource = '#form';
     loadResource(url, resource);
+    $('#form').show(); 
 }
 $('.btn-new').click( (event)=>btnClick(event) );
-$('.btn-edit').click( (event)=>btnClick(event) );
-$('.btn-list-all').click( (event)=>btnClick(event) );
 
 $('.btn-delete').click( (event)=>{
     answer = confirm('Deseja deletar?');
