41e918714552c9d2439edbba4a9e0e4cb3d57591
anacsv <anapaula.carneiro@olist.com>
2020-08-16 23:15:51 -0300

    Replicating post to product_rating

--- templates/js/product_rating.js
+++ templates/js/product_rating.js
@@ -57,4 +57,21 @@ function findById(id){
     });
 }
 // ============ END Find id in Json File and Load html
+
+function save(data) {
+    debugger;
+    $.ajax({
+        type : 'POST',
+        url: product_rating_api,
+        contentType: 'application/json',
+        data: data,
+        success: () => {
+            load_data_json();
+        },
+        error: (e) => {
+            $('.msg.error.error.api').html('<h4>Erro ao acessar a api</h4>')
+        }
+    });
+}
+
 $(document).ready(()=>load_data_json());
