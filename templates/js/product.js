55d3933a4c23b68a62d7fad2ac553fc0061a8803
Matheus Reis <matheus.reis@olist.com>
2020-08-17 11:11:27 -0300

    product with find by id

--- templates/js/product.js
+++ templates/js/product.js
@@ -42,22 +42,18 @@ function load_data(data){
 // ============ Find id in Json File and Load html
 function findById(id){
     $.ajax({
-        url : product_api
+        url : product_api+id
         ,dataType : 'json'
         ,type : 'get'
         ,success: (data)=>{
-            data.forEach(e => {
-                if(e['id']==id){
-                    $("[name='id']").val(e['id']);
-                    $("[name='name']").val(e['name']);
-                    $("[name='description']").val(e['description']);
-                    $("[name='price']").val(e['price']);
-                    $("[name='gtin']").val(e['gtin']);
-                    $("[name='brand_id']").val(e['brand_id']);
-                    $("[name='product_condition_id']").val(e['product_condition_id']);
-                    $("[name='shipping_country_id']").val(e['shipping_country_id']);
-                }
-            });
+            $("[name='id']").val(data.id);
+            $("[name='name']").val(data.name);
+            $("[name='description']").val(data.description);
+            $("[name='price']").val(data.price);
+            $("[name='gtin']").val(data.gtin);
+            $("[name='brand_id']").val(data.brand_id);
+            $("[name='product_condition_id']").val(data.product_condition_id);
+            $("[name='shipping_country_id']").val(data.shipping_country_id);
         }
     });
 }
